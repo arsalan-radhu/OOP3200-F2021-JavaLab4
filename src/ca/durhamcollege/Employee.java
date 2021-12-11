e4ac1ac586cfc1295bb8d5274a89bba1a24992d6
Arsalan Radhu <arsalan.radhu@dcmail.ca>
2021-12-10 22:38:22 -0500

    Made minor changes and some automated commenting

--- src/ca/durhamcollege/Employee.java
+++ src/ca/durhamcollege/Employee.java
@@ -1,4 +1,25 @@
+/*
+ * @authors: Arsalan Arif Radhu
+ * @date: 13 December, 2021
+ * @program: OOP3200 - Java Lab 4
+ * @description: Abstract superclass that inherits from the parent class
+ */
+
 package ca.durhamcollege;
 
-public class Employee {
+import java.time.LocalDate;
+
+public class Employee extends Person {
+    /**
+     * Initializes a person object based on parameters.
+     *
+     * @param fullName  - the intended full name, in order, separated by spaces.
+     * @param birthDate - the intended birth date.
+     * @throws IllegalArgumentException when fullName contains less than one
+     *                                  character or when fullName contains something other than letters, spaces,
+     *                                  hyphens, or apostrophes.
+     */
+    public Employee(String fullName, LocalDate birthDate) {
+        super(fullName, birthDate);
+    }
 }
